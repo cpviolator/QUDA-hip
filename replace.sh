4368b977a916b7387635c009c888584f67203528
cpviolator <dmhowarth26@gmail.com>
2019-10-31 03:01:30 -0400

    comments added to replace.sh

--- replace.sh
+++ replace.sh
@@ -12,8 +12,13 @@ cp quda-cuda/quda_cuda_api.cpp quda/lib/quda_cuda_api.cpp
 cp quda-cuda/CMakeLists.txt quda/CMakeLists.txt
 
 # changes made 'by hand'
+# These are OK, just change the source code.
 sed -i "s/DevicePointer(&num_failures_d/DevicePointer((void**)num_failures_d/g" quda/lib/interface_quda.cpp
-sed -i "s/qudaEventCreate/qudaEventCreateWithFlags/g" quda/lib/lattice_field.cpp
-sed -i "s/qudaEventQuery/cudaEventQuery/g" quda/lib/reduce_quda.cu
 sed -i "s/qudaHostGetDevicePointer(&hd_reduce/qudaHostGetDevicePointer((void**)hd_reduce/g" quda/lib/reduce_quda.cu
+
+# Possible issue with qudaEventCreate -> qudaEventCreateWithFlags
+sed -i "s/qudaEventCreate/qudaEventCreateWithFlags/g" quda/lib/lattice_field.cpp
+
+# Evidently a problem!
+sed -i "s/qudaEventQuery/cudaEventQuery/g" quda/lib/reduce_quda.cu
 sed -i "s/qudaEventQuery/cudaEventQuery/g" quda/lib/multi_reduce_quda.cu
